b89ea9de389c8e6b9355491c37d69226c90ec86d
Jason Allen <jason@jasonallen.us>
2026-09-16 14:34:16 -0400

    Fix GitHub Pages asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasonallen.us',
-  base: '/',
+  base: '/jasonallen-build/',
   trailingSlash: 'ignore'
 });
